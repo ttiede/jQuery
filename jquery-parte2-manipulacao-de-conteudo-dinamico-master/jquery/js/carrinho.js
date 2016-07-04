220e8fb6da82149f8b8403e6e3b292ea0407e851
Tiago Tiede <geschopf@gmail.com>
2016-07-04 10:41:14 -0300

    Create banner system

--- jquery-parte2-manipulacao-de-conteudo-dinamico-master/jquery/js/carrinho.js
+++ jquery-parte2-manipulacao-de-conteudo-dinamico-master/jquery/js/carrinho.js
@@ -2,6 +2,11 @@ var atualizaDados = function(){
   var carrinhos = $(".carrinho");
   $.each(carrinhos, function() {
     var carrinho = $(this);
+
+    $("tr:nth-child(3n)").each(function(){
+      umaPropaganda().insertAfter($(this));
+    });
+
     var items = carrinho.find(".item-total:visible");
     var total = 0;
     var quantidade_de_itens = 0;
@@ -15,6 +20,19 @@ var atualizaDados = function(){
 
   });
 };
+
+var umaPropaganda = function(){
+    var propagandas = ["O que acha de comprar uma motocicleta?",
+               "O que acha de comprar uma lancha?",
+               "O que acha de comprar uma bicicleta?",
+               "O que acha de comprar uma carro?"
+               ];
+    var posicao = Math.floor(propagandas.length *Math.random());
+    var texto = propagandas[posicao];
+    var tr =$("<tr>").addClass("propaganda").append($("<td>"));
+    tr.find("td").attr("colspan", 6).text(texto);
+    return tr;
+}
 
 var removeItem = function( object ) {
   //var itemTotal = self.closest("td").siblings(".item-total");
@@ -44,6 +62,13 @@ var aposInicializado = function() {
     event.preventDefault();
     undo();
   });
+  $.each(carrinhos, function() {
+    var carrinho = $(this);
+    $("tr:nth-child(3n)").each(function(){
+      umaPropaganda().insertAfter($(this));
+    });
+});
+
 };
 
  $(aposInicializado);
